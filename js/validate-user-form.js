458a96f7d14fee276874095d06b586d9d3e17ef0
Nikita Kirin <kirinnikita1406@gmail.com>
2023-08-06 22:39:49 +0500

    [TASK]: Add flash messages of form validation

--- js/validate-user-form.js
+++ js/validate-user-form.js
@@ -2,10 +2,8 @@ import {hasDuplicates} from './util.js';
 
 const form = document.querySelector('#upload-select-image');
 
-const pristine = new Pristine(form);
-
 function validateHashtags(value) {
-  if (value === '') {
+  if (value.trim() === '') {
     return true;
   }
   const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
@@ -26,13 +24,16 @@ function validateComment(value) {
   return value.length >= 0 && value.length <= 140;
 }
 
-pristine.addValidator(form.querySelector('.text__hashtags'), validateHashtags);
-pristine.addValidator(form.querySelector('.text__description'), validateComment);
-
-form.addEventListener('submit', (evt) => {
-  if (!pristine.validate()) {
-    evt.preventDefault();
-  }
-});
-
-
+if (form) {
+  const pristine = new Pristine(form, {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper'
+  });
+  pristine.addValidator(form.querySelector('.text__hashtags'), validateHashtags, 'Invalid hashtags', 1);
+  pristine.addValidator(form.querySelector('.text__description'), validateComment, 'Invalid comment', 1);
+  form.addEventListener('submit', (evt) => {
+    if (!pristine.validate()) {
+      evt.preventDefault();
+    }
+  });
+}
